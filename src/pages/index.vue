131f821034117bd3f2ec4d5845923eb4ecd040c1
yvbopeng <2032806273@qq.com>
2023-08-02 22:29:29 +0800

    init

---
+++ src/pages/index.vue
@@ -0,0 +1,24 @@
+<script setup>
+import {ref} from 'vue'
+import { useRouter } from 'vue-router'
+defineOptions({
+  name: 'IndexPage',
+})
+
+const name = ref('')
+
+const router = useRouter()
+router.push('/home')
+function go() {
+  if (name.value)
+    router.push(`/hi/${encodeURIComponent(name.value)}`)
+}
+</script>
+
+<template>
+  <div>
+    <button @click="go">
+      Go
+    </button>
+  </div>
+</template>
